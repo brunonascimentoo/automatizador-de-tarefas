45096534c75a535a7aed81e05b31142b03343ff2
Bruno Nascimento <bnascimentos1996@gmail.com>
2024-07-13 22:58:15 -0300

    primeiro commit, criação do projeto + import das dependencias e criação das variáveis

---
+++ auxiliar.py
@@ -0,0 +1,8 @@
+#Usar código auxiliar para saber posição da tela
+import time
+
+time_wait = time
+time_wait.sleep(3)
+
+screen_position = pgui.position #saber posição da tela
+print(screen_position())
---
+++ main.py
@@ -0,0 +1,16 @@
+#abrir arquivo crud
+#ler base de dados
+#cadastrar itens
+#repetir processo
+
+import pyautogui as pgui
+
+
+
+
+click = pgui.click #click na tela
+hotkey = pgui.hotkey #clica uma combição de teclas ex: ctrl + c 
+move_to = pgui.moveTo # move o mouse para local indicadohello world
+pause = pgui.PAUSE = 0.5 # pause de x segundos antes de executar algum comando
+write = pgui.write
+
